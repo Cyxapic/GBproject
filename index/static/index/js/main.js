427283b68b5c1bb61cbdde68c2c0d9f9167fdacc
Cyxapic <truecyxapic@yandex.ru>
2018-09-16 19:05:48 +0300

    ADD JS menu

--- index/static/index/js/main.js
+++ index/static/index/js/main.js
@@ -1,6 +1,20 @@
+const btnMenu = document.querySelector('.mob-btn');
+btnMenu.addEventListener("click", show_menu, false);
+
+function show_menu(){
+    const menu = document.querySelector('#main-menu');
+    const content = document.querySelector('#main-content');
+    if (menu.style.display === 'none' || menu.style.display === ''){
+        menu.style.display = 'block';
+        content.style.display = 'none';
+    } else {
+        menu.style.display = 'none';
+        content.style.display = 'block';
+    }
+};
 function show_modal(selector) {
     document.querySelector(selector).style.display = "block";
-}
+};
 function hide_modal(selector){
     document.querySelector(selector).style.display = "none";
-}
+};
